b0e5730a753cc685904c5c67d4c0db472fb553e9
Najad Nabil <najadnabil2612@gmail.com>
2026-05-29 11:34:57 +0530

    Added ADD STUDENT feature

--- students.py
+++ students.py
@@ -6,3 +6,25 @@ try:
 except:
     students = []
 file.close()
+
+admNumber = 2
+
+def addStudent():
+    global admNumber
+    name = input("Enter the name of the student : ")
+    age = int(input("Enter age of the student : "))
+    mark = int(input("Enter the total marks : "))
+
+    student = {
+        "name": name,
+        "age": age,
+        "mark": mark,
+        "id": admNumber + 1
+    }
+
+    admNumber += 1
+    students.append(student)
+
+    file = open("data.json", "w")
+    json.dump(students, file)
+    file.close()
